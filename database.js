d2b4c457844adcc970ce538ac090e58873dd3a86
x1Wolf101x <amsorensen33@gmail.com>
2025-03-26 19:44:51 -0700

    3-26-2025 Update 2
    I am struggling with learning Javascript, node.js, and express.js. I am not sure if some of the node stuff I am downloading is even working.

--- database.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2';
-
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: '360db'
-
-}).promise;
--- index.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const mysql2 = require('mysql2');
-
-const app = express();
-const port = process.env.PORT || 5000;

0b8897197df0b5f1a16ad90732371882bc81f07d
x1Wolf101x <amsorensen33@gmail.com>
2025-03-28 17:27:26 -0700

    3-28-2025 Update 1
    Signing up works (sort of)

--- app.js
+++ app.js
@@ -3,34 +3,25 @@ const bodyParser = require('body-parser');
 const database = require('./database');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/signup', (req, res) => {
-    const { first_name, last_name, city, state, address, zipcode, email, password } = req.body;
-    const query = `INSERT INTO users (first_name, last_name, city, state, address, zipcode, email, password) 
-                   VALUES ('${first_name}', '${last_name}', '${city}', '${state}', '${address}', '${zipcode}', '${email}', '${password}')`;
-    database.query(query, (err, results) => {
-        if (err) throw err;
-        res.redirect('/');
-    });
-});
+app.get('/', (req, res) => { 
+    console.log('index.ejs loaded successfully');
+    res.render('index.ejs');
 
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const query = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
-    database.query(query, (err, results) => {
-        if (err) throw err;
-        if (results.length > 0) {
-            res.redirect('/home');
-        } else {
-            res.send('Invalid login credentials');
-        }
-    });
-});
+})
+
+
 
 app.get('/home', (req, res) => {
     res.send('Login successful');
 });
+
+const userRouter = require("./routes/users.js");
+
+app.use("/users", userRouter);
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const database = mysql.createConnection({
-    host: '127.0.0.1:3306',
+    host: '127.0.0.1',
     user: 'root',
     password: '',
     database: '360db'

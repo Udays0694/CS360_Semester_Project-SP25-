d462d68d71c82186bf041e7c32635ecd208058c6
x1Wolf101x <amsorensen33@gmail.com>
2025-04-09 11:00:20 -0700

    4-9-2025 Update 2
    Backend re-added into 'Server' folder. Backend now runs on Port 5000. Has same functionality as before. Functionality will be expanded upon later today.

---
+++ Server/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+const database = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: '360db'
+});
+
+database.connect((err) => {
+    if (err) throw err;
+    console.log('Database Connected');
+});
+
+module.exports = database;
---
+++ Server/routes/users.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const database = require('../database');
+
+const router = express.Router();
+
+router.get("/signUp", (req, res) => {
+    console.log('signUp.ejs loaded successfully');
+    res.render('users/signUp');
+});
+
+router.get("/logIn", (req, res) => {
+    console.log('login.ejs loaded successfully');
+    res.render('users/login');
+});
+
+// router.post('/',)
+
+router.post('/userSignUp', (req, res) => {
+    console.log("User Created");
+    
+    const { first_name, last_name, city, state, address, zip_code, email, password } = req.body;
+    const query = `INSERT INTO users (first_name, last_name, city, state, address, zip_code, email, password) 
+                   VALUES ('${first_name}', '${last_name}', '${city}', '${state}', '${address}', '${zip_code}', '${email}', '${password}')`;
+    database.query(query, (err, results) => {
+        if (err) throw err;
+        res.redirect('/');
+    });
+});
+
+router.post('/userLogin', (req, res) => {
+    console.log("User Login Access");
+    const { email, password } = req.body;
+    const query = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
+    database.query(query, (err, results) => {
+        if (err) throw err;
+        if (results.length > 0) {
+            res.redirect('/home');
+        } else {
+            res.send('Invalid login credentials');
+        }
+    });
+});
+
+module.exports = router;

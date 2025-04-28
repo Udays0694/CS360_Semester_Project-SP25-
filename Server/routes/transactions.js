d3e48a907421f845cc890c98ec822a4996a5bfe5
x1Wolf101x <amsorensen33@gmail.com>
2025-04-28 11:27:46 -0700

    4-28-2025 Backend Update 1
    Added .ejs pages for admin dashboard.
    
    Added new routes for use in admin dashboard. Many are copied and slightly modified from existing routes.
    
    Transactions routes created, though not functional.

--- Server/routes/transactions.js
+++ Server/routes/transactions.js
@@ -10,8 +10,42 @@ router.get('/', (req, res) =>{
 });
 
 router.post('/createTransaction', (req, res)=>{
-    console.log("Transactions route not finished");
 
+    const {user_1_id, item_1_id, user_2_id, item_2_id, equivalence} = req.body;
+    const queryCreateTrans = `INSERT INTO transactions (user_1_id, item_1_id, user_2_id, item_2_id, equivalence)
+                   VALUES ('${user_1_id}', '${item_1_id}', '${user_2_id}', '${item_2_id}', '${equivalence}')`;
+    //console.log("Transactions route not finished");
+
+    database.query(queryCreateTrans, (err, results)=>{
+        if(err){
+            throw err;
+            console.log("Error creating Transaction");
+        }else{
+            console.log("Transaction Created");
+        }
+
+    });
+
+    
+    res.json(req.body);
+
+});
+
+router.post('/deleteTransaction', (req, res)=>{
+
+    const queryDelTrans = `DELETE FROM transactions WHERE transaction_id = '${req.params.transaction_id}'`;
+
+    database.query(queryDelTrans, (err, results)=>{
+        if(err){
+            console.log("Problem deleting Transaction");
+            throw err;
+        }else{
+            console.log("Transaction Deleted");
+        }
+
+    });
+
+    res.json(req.body);
 });
 
 module.exports = router;
--- Server/server.js
+++ Server/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.set('view engine', 'ejs');
 
-//Server Routers
+//Load index.ejs (Admin Dashboard Home Page)
 
 app.use(bodyParser.urlencoded({ extended: false })); 
     app.get('/', (req, res) => { 
@@ -16,16 +16,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 })
 
+//Server Routers
+
 const userRouter = require("./routes/users.js");
 const listingsRouter = require("./routes/listings.js");
 const transactionsRouter = require("./routes/transactions.js");
+const adminRouter = require("./routes/admin.js");
 
 app.use("/users", userRouter);
 app.use("/listings", listingsRouter);
 app.use("/transactions", transactionsRouter);
+app.use("/admin", adminRouter);
 
 app.get('/home', (req, res) => {
-    res.send('Login successful');
+    res.send('Login successful'); 
 });
 
 app.get('/api', (req, res) => {
